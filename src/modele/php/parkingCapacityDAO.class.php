d7888d121be4cc8f0be5eb7bb1031b92ad6ab50c
Sashiro <73870858+Sash1ro@users.noreply.github.com>
2025-11-18 11:35:40 +0100

    Fix require path in parkingCapacityDAO class

--- src/modele/php/parkingCapacityDAO.class.php
+++ src/modele/php/parkingCapacityDAO.class.php
@@ -10,7 +10,7 @@ class ParkingCapacityDAO
 
     public function __construct()
     {
-        require_once "../../modele/connexion.php";
+        require_once "../../modele/php/connexion.php";
         $this->bd = new Connexion();
         $this->select = "SELECT * FROM parking_capacity";
     }
