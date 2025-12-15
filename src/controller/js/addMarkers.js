7c9242c0e9b17376fa6d6e84c0991f8c311a5a7d
Sash1ro <matteoplancher@icloud.com>
2025-12-15 07:49:35 +0100

    fix

--- src/controller/js/addMarkers.js
+++ src/controller/js/addMarkers.js
@@ -1,6 +1,6 @@
 import { getGoogleLibs } from "./googleAPI.js";
 
-//Creer un marqueur sur la map
+//Créer un marqueur sur la map
 export async function addMarker(builder, pos, message, iconURL) {
   const { AdvancedMarkerElement } = getGoogleLibs();
 
