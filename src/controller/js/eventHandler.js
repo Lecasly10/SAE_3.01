d947d3969d6636693f4c12a652daee42201d89e1
Sash1ro <matteoplancher@icloud.com>
2026-01-01 11:44:09 +0100

    finishing edit car

--- src/controller/js/eventHandler.js
+++ src/controller/js/eventHandler.js
@@ -414,7 +414,7 @@ export function createHandlers(builder, navigation, user) {
 
     let id;
     let errors = [];
-    if (listvoit.value === "none" || listvoit.value === "") id = null;
+    if (listvoit.value === "none" || listvoit.value === "") id = user.userId;
     else id = JSON.parse(listvoit.value).id;
 
     if (isEmpty(plateParam.value))
