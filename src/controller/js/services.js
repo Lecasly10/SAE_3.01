cda6c1aacda3dfbe7b4276c864a3252eac7aff3f
Sash1ro <matteoplancher@icloud.com>
2026-01-09 12:51:19 +0100

    updating fct for errors handling

--- src/controller/js/services.js
+++ src/controller/js/services.js
@@ -6,7 +6,7 @@ import { VehiculeService } from "./vehicule/vehiculeService.js";
 import { StorageService } from "./storage/storageService.js";
 import { ApiService } from "./api/apiService.js";
 import { AppError } from "./errors/errors.js";
-
+import { UI } from "./ui/UI.js";
 //APP SERVICES
 export class Services {
     constructor() {
@@ -41,6 +41,8 @@ export class Services {
         this.userService = new UserService(this.apiService);
         this.user = this.userService.user;
         await this.userService.init();
+
+        if (this.user.isLogged) UI.toggleAuthIcon(true);
 
         //VEHICULE
         this.vehiculeService = new VehiculeService(this.user, this.apiService);
